b3f976e9b56d134317d5e403875c5a07afb186c5
Zappar <hello@zappar.com>
2021-02-11 12:51:57 +0000

    Example for Zappar ThreeJS

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import * as THREE from 'three';
+import * as ZapparThree from '@zappar/zappar-threejs';
+import targetImage1 from '../assets/two-targets/target1.zpt';
+import targetImage2 from '../assets/two-targets/target2.zpt';
+// ZapparThree provides a LoadingManager that shows a progress bar while
+// the assets are downloaded
+const manager = new ZapparThree.LoadingManager();
+
+// Setup ThreeJS in the usual way
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Setup a Zappar camera instead of one of ThreeJS's cameras
+const camera = new ZapparThree.Camera();
+
+// The Zappar library needs your WebGL context, so pass it
+ZapparThree.glContextSet(renderer.getContext());
+
+// Create a ThreeJS Scene and set its background to be the camera background texture
+const scene = new THREE.Scene();
+scene.background = camera.backgroundTexture;
+
+// Request the necessary permission from the user
+ZapparThree.permissionRequestUI().then((granted) => {
+  if (granted) camera.start();
+  else ZapparThree.permissionDeniedUI();
+});
+
+// Set up our image tracker groups
+// Pass our loading manager in to ensure the progress bar works correctly
+const tracker1 = new ZapparThree.ImageTrackerLoader(manager).load(targetImage1);
+const tracker2 = new ZapparThree.ImageTrackerLoader(manager).load(targetImage2);
+
+const trackerGroup1 = new ZapparThree.ImageAnchorGroup(camera, tracker1);
+const trackerGroup2 = new ZapparThree.ImageAnchorGroup(camera, tracker2);
+scene.add(trackerGroup1);
+scene.add(trackerGroup2);
+// Add some content
+const box1 = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(),
+  new THREE.MeshBasicMaterial(),
+);
+const box2 = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(),
+  new THREE.MeshBasicMaterial(),
+);
+box1.position.set(0, 0, 0.5);
+box2.position.set(0, 0, 0.5);
+
+trackerGroup1.add(box1);
+trackerGroup2.add(box2);
+
+// Set up our render loop
+function render() {
+  requestAnimationFrame(render);
+  camera.updateFrame(renderer);
+
+  renderer.render(scene, camera);
+}
+
+requestAnimationFrame(render);
